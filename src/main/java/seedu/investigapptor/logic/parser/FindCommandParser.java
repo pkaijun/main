b0bdc6791a1c0114a75aaf261224ef98c2476fe9
Poh Kai Jun <kai_jun96@hotmail.com>
2018-03-29 12:09:20 +0800

    Modify parser to separate command with whitespaces

--- src/main/java/seedu/investigapptor/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/investigapptor/logic/parser/FindCommandParser.java
@@ -1,9 +1,13 @@
 package seedu.investigapptor.logic.parser;
 
 import static seedu.investigapptor.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.investigapptor.logic.parser.ParserKeywords.KEYWORD_INVEST;
+import static seedu.investigapptor.logic.parser.ParserKeywords.KEYWORD_CASE;
+import static seedu.investigapptor.logic.parser.ParserKeywords.KEYWORD_TAGS;
 
 import java.util.Arrays;
 
+import seedu.investigapptor.logic.commands.Command;
 import seedu.investigapptor.logic.commands.FindCommand;
 import seedu.investigapptor.logic.parser.exceptions.ParseException;
 import seedu.investigapptor.model.person.NameContainsKeywordsPredicate;
@@ -11,14 +15,14 @@ import seedu.investigapptor.model.person.NameContainsKeywordsPredicate;
 /**
  * Parses input arguments and creates a new FindCommand object
  */
-public class FindCommandParser implements Parser<FindCommand> {
+public class FindCommandParser implements Parser<Command> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
      * and returns an FindCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public FindCommand parse(String args) throws ParseException {
+    public Command parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
@@ -26,8 +30,15 @@ public class FindCommandParser implements Parser<FindCommand> {
         }
 
         String[] nameKeywords = trimmedArgs.split("\\s+");
+        String type = nameKeywords[0];
 
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        switch(type) {
+            case KEYWORD_INVEST:
+                return new FindInvestTagsCommandParser().parse(args);
+            default:
+                return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        }
+
     }
 
 }
---
+++ src/main/java/seedu/investigapptor/logic/parser/ParserKeywords.java
@@ -0,0 +1,13 @@
+//@@author pkaijun
+package seedu.investigapptor.logic.parser;
+
+/**
+ Keywords file for command parsing
+ **/
+public class ParserKeywords {
+
+    public static final String KEYWORD_INVEST = "invest";
+    public static final String KEYWORD_CASE = "case";
+    public static final String KEYWORD_TAGS = "tag";
+
+}
